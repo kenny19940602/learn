3e3cd4df7ba0f6f335ca7fb2e82b1115a3815f55
kenny <kenny19940602@163.com>
2025-07-15 14:48:51 +0800

    [add]  增加jpa

--- src/main/java/link/jinlong/learn/CashCard.java
+++ src/main/java/link/jinlong/learn/CashCard.java
@@ -1,7 +1,9 @@
 package link.jinlong.learn;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public record CashCard(@Id Long id, Double amount, String owner) {
 
 }
--- src/main/java/link/jinlong/learn/LearnApplication.java
+++ src/main/java/link/jinlong/learn/LearnApplication.java
@@ -1,13 +1,55 @@
 package link.jinlong.learn;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import link.jinlong.learn.accessingdatajpa.Customer;
+import link.jinlong.learn.accessingdatajpa.CustomerRepository;
 
 @SpringBootApplication
 public class LearnApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(LearnApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            log.info("Customers found with findAll():");
+            log.info("-----------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            Optional<Customer> customer = repository.findById(1L);
+            log.info("Customer found with findById(1L):");
+            log.info("--------------------------------");
+            log.info(customer.get().toString());
+            log.info("");
+
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+            log.info("");
+        };
     }
 
 }
